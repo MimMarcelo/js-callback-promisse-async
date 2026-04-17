a38d20ec14c91e18af49fbb437d4bc37d99a30d3
Marcelo Júnior <rokermarcelo@gmail.com>
2026-04-17 17:30:27 -0300

    Mudança do local do settimeout em promises/contagem.js

--- js/promises/contagem.js
+++ js/promises/contagem.js
@@ -1,34 +1,32 @@
-botao.onclick = function(){
+botao.onclick = function () {
     mensagem.innerText = "3";
 
     mostrar(2)
-    .then((msg)=>{ // Imprime 2
-        mensagem.innerText = msg;
-        return mostrar(msg-1);
-    })
-    .then((msg)=>{ // Imprime 1
-        mensagem.innerText = msg;
-        return mostrar(msg-1);
-    })
-    .then((msg)=>{ // Vai ser rejeitado
-        mensagem.innerText = msg;
-        return mostrar(msg-1);
-    })
-    .catch((msg_final) =>{
-        mensagem.innerText = msg_final;
-    });
+        .then((msg) => { // Imprime 2
+            mensagem.innerText = msg;
+            return mostrar(msg - 1);
+        })
+        .then((msg) => { // Imprime 1
+            mensagem.innerText = msg;
+            return mostrar(msg - 1);
+        })
+        .then((msg) => { // Vai ser rejeitado
+            mensagem.innerText = msg;
+            return mostrar(msg - 1);
+        })
+        .catch((msg_final) => {
+            mensagem.innerText = msg_final;
+        });
 }
 
 function mostrar(numero) {
     return new Promise((resolve, reject) => {
-        if(numero >= 0){
-            setTimeout(() => {
+        setTimeout(() => {
+            if (numero >= 0)
                 resolve(numero);
-            }, 1000);
-        }
-        else{
-            reject("Pronto!");
-        }
+            else
+                reject("Pronto!");
+        }, 1000);
     })
 }
 
